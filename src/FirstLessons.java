0c6b19e3da9ad5f4e947431a70a6e97487eb6872
Daniel <drobin897@gmail.com>
2017-11-25 13:42:51 -0500

    Initial Commit

---
+++ src/FirstLessons.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class FirstLessons {
+
+	public static void main(String[] args) {
+		Random generator = new Random();	
+		int i = generator.nextInt(10);
+		System.out.println(i);
+		
+		i = generator.nextInt(10);
+		System.out.println(i);
+		
+		i = generator.nextInt(10);
+		System.out.println(i);
+		
+		
+
+	}
+
+}
