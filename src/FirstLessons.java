fd58941ac788930c7dd2231bd6953fe1a0f3d52d
Daniel <drobin897@gmail.com>
2017-11-25 14:26:12 -0500

    Added several new examples for each method

--- src/FirstLessons.java
+++ src/FirstLessons.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 public class FirstLessons {
 
 	public static void main(String[] args) {
+		/*
 		//Use of the Class Random
 		
 		Random generator = new Random();	
@@ -13,6 +14,34 @@ public class FirstLessons {
 		i = generator.nextInt(10);
 		System.out.println(i);
 		
+		//Making a random number generator that feeds into an if-else statement
+		
+		Random x = new Random();
+		int y = x.nextInt(4);
+		System.out.println(y);
+		
+		if (y == 0)
+		{
+			System.out.println("Only 0. That's not enough!");
+		}
+		else if (y == 1)
+		{
+			System.out.println("Only 1. That's not enough!");
+		}
+		else if (y == 2)
+		{
+			System.out.println("2 points! You passed!");
+		}
+		else if (y == 3)
+		{
+			System.out.println("3 points, perfect!");
+		}
+		else
+		{
+			System.out.println("Somehow the random number generator has gone askew! Has it gained counsciousness and thus autonomy?!");
+		}
+		
+		 
 		//Reversing a String
 		
 		String forward = "This is a test!";
@@ -28,6 +57,26 @@ public class FirstLessons {
 		System.out.println(forward);
 		System.out.println(reverse);
 		
+ 
+		
+		//Using Stringbuilder and Reverse method to take in user data and output the reverse of this data
+		
+		System.out.println("Please enter something");
+		
+		Scanner input = new Scanner(System.in);
+		String answer = input.nextLine();
+		StringBuilder build = new StringBuilder();
+		build.append(answer);
+		String revAnswer = build.reverse().toString();
+		
+		System.out.println("Here is your data...");
+		System.out.println(answer);
+		
+		System.out.println("And here is the reverse of your data!");
+		System.out.println(revAnswer);
+		
+		
+		
 		//Some new math
 		Scanner input = new Scanner(System.in);	
 		System.out.println("Enter Circle Radius : ");
@@ -37,7 +86,8 @@ public class FirstLessons {
 		double area = Math.PI * radius * radius;
 				
 		System.out.printf("Circumfrences : %f: Area %f \n", circ, area);
-
+*/
+		
 	}
 
 }
